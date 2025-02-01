10eb74447c844d4c95b8c50c07ab5fae4669b6ed
anhviet.dev <vanh23689@gmail.com>
2025-02-01 09:25:00 +0700

    initial commit 2025

---
+++ main.js
@@ -0,0 +1,14 @@
+function hi2025() {
+    const newYears = {
+        a: 'Happy new year 2025',
+        b: 'All the best',
+        c: 'Everything is successful',
+        d: 'Work is smooth',
+        e: 'High salary',
+        f: 'Excellent foreign language, communication, listening and speaking can be understood'
+    };
+
+    console.log(newYears.a + '\n' + newYears.b + '\n' + newYears.c + '\n' + newYears.d + '\n' + newYears.e + '\n' + newYears.f);
+}
+
+hi2025();
